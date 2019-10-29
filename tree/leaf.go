d90ac31ee653c7b2e1f650084549bf2a54d40005
DerAlexx <alexweb199@hotmail.de>
2019-10-29 23:13:54 +0100

    Added Leafs and Node and Basic Actor

---
+++ tree/leaf.go
@@ -0,0 +1,61 @@
+package tree
+
+/*
+Leaf is the representation of the LEAF of a Node at
+the end of a treebranch
+*/
+type Leaf struct {
+	Data map[int]string
+}
+
+/*
+NewLeaf will return a new Leaf
+*/
+func NewLeaf() Leaf {
+	return Leaf{
+		Data: make(map[int]string),
+	}
+}
+
+/*
+Contains will check whether or not a key is already in the map
+@return will return true in case the key is in the map otherwise false
+*/
+func (l *Leaf) Contains(key int) bool {
+	_, ok := l.Data[key]
+	return ok
+}
+
+/*
+Insert will insert a pair into the map incase it is not already
+in the map.
+@return will return a bool depending on the insert, if it can insert the pair
+it will return true otherwise false
+*/
+func (l *Leaf) Insert(key int, value string) bool {
+	if !l.Contains(key) {
+		l.Data[key] = value
+		return true
+	}
+	return false
+}
+
+/*
+Size will return the size of map of the leaf,
+so who many paris are stored in the map
+@return will be an int value
+*/
+func (l *Leaf) Size() int {
+	return len(l.Data)
+}
+
+/*
+Erase will erase a given key from
+*/
+func (l *Leaf) Erase(key int) bool {
+	if l.Contains(key) {
+		delete(l.Data, key)
+		return true
+	}
+	return false
+}
