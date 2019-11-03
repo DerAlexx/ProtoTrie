2ae870277ae6d8a67cd1e59a514dfb81bf6a8899
DerAlexx <alexweb199@hotmail.de>
2019-11-03 18:35:52 +0100

    Added some more function for behavior changing

--- tree/node.go
+++ tree/node.go
@@ -27,6 +27,20 @@ type Insertmessage struct {
 }
 
 /*
+DeleteMessage will
+*/
+type DeleteMessage struct {
+	Key int
+}
+
+/*
+ChangeValueMessage will
+*/
+type ChangeValueMessage struct {
+	Element Pair
+}
+
+/*
 Pair r
 */
 type Pair struct {
@@ -38,7 +52,7 @@ type Pair struct {
 Nodeactor is
 */
 type Nodeactor struct {
-	behavior     actor.behavior
+	behavior     actor.Behavior
 	LeftElement  interface{}
 	RightElement interface{}
 }
@@ -46,7 +60,7 @@ type Nodeactor struct {
 /*
 CreateBasicNode will return a Basic node containing the parentnode and a left, right leaf.
 */
-func CreateBasicNode() *Nodeactor {
+func CreateBasicNode() Nodeactor {
 	return Nodeactor{
 		LeftElement:  NewLeaf(),
 		RightElement: NewLeaf(),
@@ -54,11 +68,32 @@ func CreateBasicNode() *Nodeactor {
 }
 
 /*
+Insert will insert a pair(key/value) into the correct leafs.
+*/
+func (state *Nodeactor) Insert(pair Pair) (bool, error) {
+	return false, nil
+}
+
+/*
+Delete a entry from the Trie.
+*/
+func (state *Nodeactor) Delete(key int) (bool, error) {
+	return false, nil
+}
+
+/*
 StoringNodeBehavior Method to set the Behavior of a Node to a Storing Node.
 So it will have to leafs as childs and store information in this leafs.
 */
 func (state *Nodeactor) StoringNodeBehavior(context actor.Context) {
-
+	switch msg := context.Message().(type) {
+	case *Insertmessage:
+		state.Insert(msg.Element)
+	case *DeleteMessage:
+		state.Delete(msg.Key)
+	case *ChangeValueMessage:
+		state.ChangeValueMessage(msg.Element)
+	}
 }
 
 /*
@@ -66,7 +101,10 @@ KnownNodeBehavior Method to set the Behavoir of a Node to a Knowing Node.
 So it will have to nodes as childs and know's about this childs/manged them.
 */
 func (state *Nodeactor) KnownNodeBehavior(context actor.Context) {
-
+	switch msg := context.Message().(type) {
+	case *Insertmessage:
+		fmt.Println(msg)
+	}
 }
 
 /*
@@ -81,7 +119,7 @@ func (state *Nodeactor) Receive(context actor.Context) {
 		fmt.Println("42")
 
 	case *Insertmessage:
-		fmt.Println(msg)
+		fmt.Println(msg.Element.Key)
 		n := Nodeactor{
 			LeftElement:  12,
 			RightElement: "Hello",
