dab1079b7fd4bc55408732566aad986be11b6dde
DerAlexx <alexweb199@hotmail.de>
2019-11-17 14:25:35 +0100

    Added some more stuff and fixed some errors

--- tree/node.go
+++ tree/node.go
@@ -104,7 +104,12 @@ type Nodeactor struct {
 	Limit        int
 }
 
-func 
+/*
+nodeAndLeafLogger will be a logger for the Servcie to follow along what happens in the nodes and leafs.
+*/
+func nodeAndLeafLogger(message string) {
+	//TODO missing
+}
 
 /*
 CreateBasicNode will return a Basic node containing the parentnode and a left, right leaf.
